37bf447d1da48deaa3ebe89d630ec15067140e97
gajju <gajjuparmar50@gmail.com>
2026-05-25 13:55:25 +0530

    learn react -flow

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai";
+
+
+function App() {
+ 
+
+  return (
+
+    <Chai/>
+    
+  )
+// you can return only one element in react but that element can have multiple child elements
+}
+export default App
